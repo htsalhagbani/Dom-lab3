910b0b5e305b32e5ab268d17916174a482f99703
Haneen <htsalhagbani@gmail.com>
2024-09-12 11:01:08 +0300

    add solution

---
+++ main.js
@@ -0,0 +1,8 @@
+let divbutoon = document.getElementById('htn1');
+divbutoon.classList.add('.divbutoon');
+
+let divbutoon2 = document.getElementById('htn2');
+divbutoon.classList.add('.divbutoon2');
+
+let divbutoon3 = document.getElementById('htn3');
+divbutoon.classList.add('.divbutoon3');
